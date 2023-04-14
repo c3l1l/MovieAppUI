b1dea531d1386693e6231725017c835dfa8c5f00
Celil Ozturk <celil.ozturk@windowslive.com>
2023-04-14 03:23:41 +0200

    Dashboard

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { MovieService } from '../services/movie.service';
+import { MovieDetailService } from '../services/movie-detail.service';
+import { MovieModel } from '../models/movie-model';
+import { DirectorService } from '../services/director.service';
+import { DirectorModel } from '../models/director-model';
+import { MovieInfoModel } from 'src/app/ui/models/movie-info-model';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +12,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+
+  movieList:MovieModel[]=[];
+  directorList:DirectorModel[]=[];
+  movieInfoList:MovieInfoModel[]=[];
+
+constructor(private movieService:MovieService,private directorService:DirectorService,private movieDetailService:MovieDetailService){
+
+}
+ngOnInit(){
+  this.getLastAddedMovies();
+  this.getPopularDirectors();
+  this.getAllMoviesWithActorsAndDirector();
+}
+getLastAddedMovies(){
+    this.movieService.getAllMoviesWithActorsAndDirector().subscribe((res:any)=>{
+      console.log(res.data);
+      this.movieList=res.data;
+    })
+}
+getPopularDirectors(){
+this.directorService.getAll().subscribe((res:any)=>{
+  this.directorList=res.data;
+})
+}
+
+getAllMoviesWithActorsAndDirector(){
+  this.movieService.getAllMoviesWithActorsAndDirector().subscribe((res:any)=>{
+    this.movieInfoList=res.data
+    console.log(res);
+    this.getAllMoviePosterPath();
+  })
+}
+getAllMoviePosterPath(){
+  this.movieInfoList.forEach(m => {
+    this.movieDetailService.getByMovieId(Number(m.id)).subscribe((res:any)=>{
+       m.posterPath= res.data.posterPath;
+     })
+  });
+}
+
 
 }
